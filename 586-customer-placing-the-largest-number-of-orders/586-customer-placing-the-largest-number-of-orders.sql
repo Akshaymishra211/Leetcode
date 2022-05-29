79035daf11ee1d9fec9eb502feef715b18d4aecb
Akshay Mishra <51031884+Akshaymishra211@users.noreply.github.com>
2022-05-29 17:57:32 +0530

    Time: 456 ms (63.27%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -1,2 +1,2 @@
-/* Write your T-SQL query statement below */
-select customer_number from orders group by customer_number having count(customer_number) in (select max(x.num) from (select count(t.customer_number) as num from Orders as t group by t.customer_number) as x);
+# Write your MySQL query statement below
+select customer_number from orders group by customer_number having count(customer_number)=(select max(x.num) from (select count(t.customer_number) as num from Orders as t group by t.customer_number) as x);
