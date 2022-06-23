0696f904c675020f211b6f5a564f3379e74cc11b
Akshay Mishra <51031884+Akshaymishra211@users.noreply.github.com>
2022-06-23 09:06:30 +0530

    Time: 5 ms (97.39%), Space: 41.6 MB (98.81%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -2,20 +2,18 @@ class Solution {
     int[] dirs = {0, 1, 0, -1, 0};
     public int shortestPath(int[][] grid, int k) {
         int m = grid.length, n = grid[0].length;
-        if (k >= m+n-2) return m+n-2; // if we can go by manhattan distance -> let's go
-
+        if (k >= m+n-2) return m+n-2;
         boolean[][][] visited = new boolean[m][n][k+1];
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0, k, 0}); // r, c, k, dist
+        q.offer(new int[]{0, 0, k, 0});
         visited[0][0][k] = true;
-
         while (!q.isEmpty()) {
             int[] top = q.poll();
             int r = top[0], c = top[1], curK = top[2], dist = top[3];
-            if (r == m - 1 && c == n - 1) return dist; // Found the result
+            if (r == m - 1 && c == n - 1) return dist;
             for (int i = 0; i < 4; i++) {
                 int nr = r + dirs[i], nc = c + dirs[i + 1];
-                if (nr < 0 || nr == m || nc < 0 || nc == n) continue; // skip out of bound cells!
+                if (nr < 0 || nr == m || nc < 0 || nc == n) continue;
                 int newK = curK - grid[nr][nc];
                 if (newK >= 0 && !visited[nr][nc][newK]) {
                     visited[nr][nc][newK] = true;
@@ -23,6 +21,6 @@ class Solution {
                 }
             }
         }
-        return -1; // Not found
+        return -1;
     }
 }
