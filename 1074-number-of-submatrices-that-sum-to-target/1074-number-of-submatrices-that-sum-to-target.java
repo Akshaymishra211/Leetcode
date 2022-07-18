4caa0ac2d3da8840377bbce8809cab9a4951ad97
Akshay Mishra <51031884+Akshaymishra211@users.noreply.github.com>
2022-07-18 22:42:49 +0530

    Time: 300 ms (34.49%), Space: 117.3 MB (54.59%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -2,44 +2,57 @@ class Solution {
     public static boolean isValid(int x,int y,int n,int m){
         return x>=0 && x<n && y>=0 && y<m;
     }
-    public static int getSum(int i,int j,int n,int m,int [][]mat){
-        if(isValid(i,j,n,m)){
-            return mat[i][j];
-        }
-        return 0;
-    }
-    public static int solve(int x,int y,int n,int m,int [][]arr,int target){
-        int res=0;
-        for(int i=x;i<n;i++){
-            for(int j=y;j<m;j++){
-                if(arr[i][j]-getSum(i,y-1,n,m,arr)-getSum(x-1,j,n,m,arr)+getSum(x-1,y-1,n,m,arr)==target){
-                    res++;
-                }
-            }
-        }
-        return res;
-    }
-    public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        int n=matrix.length,m=matrix[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(isValid(i,j-1,n,m)){
-                    matrix[i][j]+=matrix[i][j-1];
-                }
-                if(isValid(i-1,j,n,m)){
-                    matrix[i][j]+=matrix[i-1][j];
-                }
-                if(isValid(i-1,j-1,n,m)){
-                    matrix[i][j]-=matrix[i-1][j-1];
-                }
-            }
-        }
+    public static int solveByCol(int[][] matrix,int target,int n,int m){
         int cnt=0;
+        Map<Integer,Integer> mp=new HashMap<>();
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                cnt+=solve(i,j,n,m,matrix,target);
+            for(int j=1;j<m;j++){
+                matrix[i][j]+=matrix[i][j-1];
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=i;j<m;j++){
+                int curr_sum=0;
+                mp.clear();
+                mp.put(0,1);
+                for(int k=0;k<n;k++){
+                    curr_sum+=matrix[k][j]-(i-1>=0?matrix[k][i-1]:0);
+                    cnt+=mp.getOrDefault(curr_sum-target,0);
+                    mp.put(curr_sum,mp.getOrDefault(curr_sum,0)+1);
+                }
             }
         }
         return cnt;
+    }
+    public static int solveByRow(int[][] matrix,int target,int n,int m){
+        int cnt=0;
+        Map<Integer,Integer> mp=new HashMap<>();
+        for(int i=0;i<m;i++){
+            for(int j=1;j<n;j++){
+                matrix[j][i]+=matrix[j-1][i];
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int curr_sum=0;
+                mp.clear();
+                mp.put(0,1);
+                for(int k=0;k<m;k++){
+                    curr_sum+=matrix[j][k]-(i-1>=0?matrix[i-1][k]:0);
+                    cnt+=mp.getOrDefault(curr_sum-target,0);
+                    mp.put(curr_sum,mp.getOrDefault(curr_sum,0)+1);
+                }
+            }
+        }
+        return cnt;
+    }
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        int n=matrix.length,m=matrix[0].length;
+        if(n>=m){
+            return solveByCol(matrix,target,n,m);
+        }
+        else{
+            return solveByRow(matrix,target,n,m);
+        }
     }
 }
