5bdf432cd2b749b021867b139d8530f9a1eaf990
Akshay Mishra <51031884+Akshaymishra211@users.noreply.github.com>
2022-08-31 13:55:15 +0530

    Time: 12 ms (44.71%), Space: 55.1 MB (44.27%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -2,83 +2,70 @@ class Solution {
     public static boolean isValid(int i,int j,int n,int m){
         return i>=0 && i<n && j>=0 && j<m;
     }
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        Queue<Pair> q=new LinkedList<>();
-        int n=heights.length,m=heights[0].length;
-        boolean atlvis[][]=new boolean[n][m];
-        for(int i=0;i<n;i++){
-            q.add(new Pair(i,0));
-            atlvis[i][0]=true;
+    public static void bfs(Queue<Pair> q,boolean[][] vis,int n,int m,int [][]heights){
+        while(!q.isEmpty()){
+            Pair p=q.poll();
+            int i=p.i,j=p.j;
+            if(isValid(i-1,j,n,m) && !vis[i-1][j] && heights[i-1][j]>=heights[i][j]){
+                q.add(new Pair(i-1,j));
+                vis[i-1][j]=true;
+            }
+            if(isValid(i+1,j,n,m) && !vis[i+1][j] && heights[i+1][j]>=heights[i][j]){
+                q.add(new Pair(i+1,j));
+                vis[i+1][j]=true;
+            }
+            if(isValid(i,j-1,n,m) && !vis[i][j-1] && heights[i][j-1]>=heights[i][j]){
+                q.add(new Pair(i,j-1));
+                vis[i][j-1]=true;
+            }
+            if(isValid(i,j+1,n,m) && !vis[i][j+1] && heights[i][j+1]>=heights[i][j]){
+                q.add(new Pair(i,j+1));
+                vis[i][j+1]=true;
+            }
         }
+    }
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int n=heights.length,m=heights[0].length;
+        Queue<Pair> q=new LinkedList<>();
+        boolean vis1[][]=new boolean[n][m];
         for(int i=0;i<m;i++){
             q.add(new Pair(0,i));
-            atlvis[0][i]=true;
+            vis1[0][i]=true;
         }
-        while(!q.isEmpty()){
-            Pair p=q.poll();
-            if(isValid(p.x-1,p.y,n,m) && !atlvis[p.x-1][p.y] && heights[p.x][p.y]<=heights[p.x-1][p.y]){
-                atlvis[p.x-1][p.y]=true;
-                q.add(new Pair(p.x-1,p.y));
-            }
-            if(isValid(p.x+1,p.y,n,m) && !atlvis[p.x+1][p.y] && heights[p.x][p.y]<=heights[p.x+1][p.y]){
-                atlvis[p.x+1][p.y]=true;
-                q.add(new Pair(p.x+1,p.y));
-            }
-            if(isValid(p.x,p.y-1,n,m) && !atlvis[p.x][p.y-1] && heights[p.x][p.y]<=heights[p.x][p.y-1]){
-                atlvis[p.x][p.y-1]=true;
-                q.add(new Pair(p.x,p.y-1));
-            }
-            if(isValid(p.x,p.y+1,n,m) && !atlvis[p.x][p.y+1] && heights[p.x][p.y]<=heights[p.x][p.y+1]){
-                atlvis[p.x][p.y+1]=true;
-                q.add(new Pair(p.x,p.y+1));
-            }
+        for(int i=1;i<n;i++){
+            q.add(new Pair(i,0));
+            vis1[i][0]=true;
         }
-        boolean pacvis[][]=new boolean[n][m];
+        bfs(q,vis1,n,m,heights);
+        q.clear();
+        boolean vis2[][]=new boolean[n][m];
+        for(int i=0;i<m-1;i++){
+            q.add(new Pair(n-1,i));
+            vis2[n-1][i]=true;
+        }
         for(int i=0;i<n;i++){
             q.add(new Pair(i,m-1));
-            pacvis[i][m-1]=true;
+            vis2[i][m-1]=true;
         }
-        for(int i=0;i<m;i++){
-            q.add(new Pair(n-1,i));
-            pacvis[n-1][i]=true;
-        }
-        while(!q.isEmpty()){
-            Pair p=q.poll();
-            if(isValid(p.x-1,p.y,n,m) && !pacvis[p.x-1][p.y] && heights[p.x][p.y]<=heights[p.x-1][p.y]){
-                pacvis[p.x-1][p.y]=true;
-                q.add(new Pair(p.x-1,p.y));
-            }
-            if(isValid(p.x+1,p.y,n,m) && !pacvis[p.x+1][p.y] && heights[p.x][p.y]<=heights[p.x+1][p.y]){
-                pacvis[p.x+1][p.y]=true;
-                q.add(new Pair(p.x+1,p.y));
-            }
-            if(isValid(p.x,p.y-1,n,m) && !pacvis[p.x][p.y-1] && heights[p.x][p.y]<=heights[p.x][p.y-1]){
-                pacvis[p.x][p.y-1]=true;
-                q.add(new Pair(p.x,p.y-1));
-            }
-            if(isValid(p.x,p.y+1,n,m) && !pacvis[p.x][p.y+1] && heights[p.x][p.y]<=heights[p.x][p.y+1]){
-                pacvis[p.x][p.y+1]=true;
-                q.add(new Pair(p.x,p.y+1));
-            }
-        }
+        bfs(q,vis2,n,m,heights);
         List<List<Integer>> res=new ArrayList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(atlvis[i][j] && pacvis[i][j]){
-                    List<Integer> list=new ArrayList<>();
-                    list.add(i);
-                    list.add(j);
-                    res.add(list);
+                if(vis1[i][j] && vis2[i][j]){
+                    List<Integer> temp=new ArrayList<>();
+                    temp.add(i);
+                    temp.add(j);
+                    res.add(temp);
                 }
             }
         }
         return res;
     }
     static class Pair{
-        int x,y;
-        public Pair(int x,int y){
-            this.x=x;
-            this.y=y;
+        int i,j;
+        public Pair(int i,int j){
+            this.i=i;
+            this.j=j;
         }
     }
 }
